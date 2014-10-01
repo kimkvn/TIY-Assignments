7a3eddcd44dfe5ebfb8807af370d50e5d7309f96
Kevin Kim <kimkvn@gmail.com>
2014-10-01 16:46:43 -0400

    @kimkvn @phillycc

--- dojo--2014-10-01.js
+++ dojo--2014-10-01.js
@@ -1,25 +1,30 @@
-function changeNum(a){
-  var arrDigits = [];
-  arrDigits = a.toString().split('');
+
+
+ function changeNum(a){
+
+ var arrDigits = [];
+ arrDigits = a.toString().split('');
+
 
   if (arrDigits.length === 1){
-    singleDigit(a);
+    return singleDigit(arrDigits[0]);
   }
-
+}
 
 
 
 
 function singleDigit(a){
-  if a===1; return "one";
-  if a===2; return "two";
-  if a===3; return "three";
-  if a===4; return "four";
-  if a===5; return "five";
-  if a===6; return "six";
-  if a===7; return "seven";
-  if a===8; return "eight";
-  if a===9; return "nine";
+console.log(a);
+  if (a==="1") {return "one";}
+  if (a==="2") {return "two";}
+  if (a==="3") {return "three";}
+  if (a==="4") {return "four";}
+  if (a==="5") {return "five";}
+  if (a==="6") {return "six";}
+  if (a==="7") {return "seven";}
+  if (a==="8") {return "eight";}
+  if (a==="9") {return "nine";}
 }
 
 
@@ -33,7 +38,3 @@ function singleDigit(a){
 
 console.log ('given the number 2, it returns "two"',
   changeNum(2) === "two");
-console.log ('given the number 5, it returns "five"',
-  changeNum(5) === "five");
-console.log ('given the number 8, it returns "eight"',
-  changeNum(8) === "eight");
