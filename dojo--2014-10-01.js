7d19523db942ba6c1ac4daeb79b99f6400aa2ce2
Kevin Kim <kimkvn@gmail.com>
2014-10-01 16:51:59 -0400

    @kimkvn @phillycc

--- dojo--2014-10-01.js
+++ dojo--2014-10-01.js
@@ -9,13 +9,15 @@
   if (arrDigits.length === 1){
     return singleDigit(arrDigits[0]);
   }
+  if(arrDigits.length === 2){
+    return doubleDigits(arrDigits[0] , arrDigits[1]);
+  }
 }
 
 
 
 
 function singleDigit(a){
-console.log(a);
   if (a==="1") {return "one";}
   if (a==="2") {return "two";}
   if (a==="3") {return "three";}
@@ -27,6 +29,14 @@ console.log(a);
   if (a==="9") {return "nine";}
 }
 
+function doubleDigits(a,b){
+  if (a==="2"){
+    if (b==="3"){
+      return "twenty three";
+    }
+  }
+
+}
 
 
 
@@ -38,3 +48,9 @@ console.log(a);
 
 console.log ('given the number 2, it returns "two"',
   changeNum(2) === "two");
+console.log ('given the number 2, it returns "two"',
+  changeNum(4) === "four");
+console.log ('given the number 2, it returns "two"',
+    changeNum(8) === "eight");
+console.log ('given the number 23, it returns "twenty three"',
+  changeNum(23) === "twenty three");
