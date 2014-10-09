1ae2364406c7789f9f2b464d2d5357f1b7932dd7
Kevin Kim <kimkvn@gmail.com>
2014-10-09 01:16:46 -0400

    established variables

--- dojo--redux-2014-10-01.js
+++ dojo--redux-2014-10-01.js
@@ -50,30 +50,30 @@ var hundreds = ["zero", "one hundred", "two hundred", "three hundred",
 "four hundred", "five hundred", "six hundred", "seven hundred",
 "eight hundred", "nine hundred"];
 
-var thousands = ["zero", "one thousand", "two thousand", "three thousand",
-"four thousand", "five thousand", "six thousand", "seven thousand",
-"eight thousand", "nine thousand"];
 
-function numChange(a){
+function toEnglish(value){
 
-  var value = a.toFixed(2);
-  var dollars = value.slice(0,-3);
-  var dollarsSplit = dollars.split("");
-  var cents = a.toFixed(2).slice(-2);
+  var valueString = value.toString();
+  var cents = valueString.slice(-2);
+  var dollars = Math.floor(value);
+  var dollarsSplit = valueString.split("")
 
-//////////0-19///////////////////////////////////////////////
-    if (a < 20){return(ones[dollars])+" and "+cents+"/100s"}; //for numbers 0-19
 
-/////////20-99/////////////////////////////////////////////////
-    if (a < 100){
-      if (a % 10 === 0){return(tens[dollarsSplit[0]]+" and "+cents+"/100s")}
-      if (ones[dollarsSplit[1]] === "zero" && cents !== 00){return(tens[dollarsSplit[0]] + " and "+cents+"/100s")}
-    return(tens[dollarsSplit[0]] + ones[dollarsSplit[1]]+ " and "+cents+"/100s")
-    }
-///////////100-999/////////////////////////////////////////////////
-    if (a < 1000){
-      if (a % 100 === 0){return(hundreds[value[0]]);}
-        if (a % 10 === 0){return(hundreds[value[0]]+tens[value[1]]);}
-          if (value[1] < 2) {return(hundreds[value[0]] + teens[value[2]]);}
-    return(hundreds[value[0]] + tens[value[1]] + ones[value[2]])
-    }
+}
+console.log(toEnglish(3.56))
+// //////////0-19///////////////////////////////////////////////
+//     if (a < 20){return(ones[dollars])+" and "+cents+"/100s"}; //for numbers 0-19
+//
+// /////////20-99/////////////////////////////////////////////////
+//     if (a < 100){
+//       if (a % 10 === 0){return(tens[dollarsSplit[0]]+" and "+cents+"/100s")}
+//       if (ones[dollarsSplit[1]] === "zero" && cents !== 00){return(tens[dollarsSplit[0]] + " and "+cents+"/100s")}
+//     return(tens[dollarsSplit[0]] + ones[dollarsSplit[1]]+ " and "+cents+"/100s")
+//     }
+// ///////////100-999/////////////////////////////////////////////////
+//     if (a < 1000){
+//       if (a % 100 === 0){return(hundreds[value[0]]);}
+//         if (a % 10 === 0){return(hundreds[value[0]]+tens[value[1]]);}
+//           if (value[1] < 2) {return(hundreds[value[0]] + teens[value[2]]);}
+//     return(hundreds[value[0]] + tens[value[1]] + ones[value[2]])
+//     }
