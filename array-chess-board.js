69f309c809dadd2fe1cf81b9fb5cf7a22b1e4a15
Kevin Kim <kimkvn@gmail.com>
2014-10-09 20:04:29 -0400

    replicated the example on MDN for moving the pawn two spaces forward.

--- array-chess-board.js
+++ array-chess-board.js
@@ -8,3 +8,10 @@ var board = [
 ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
 ['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r']
 ]; //END BOARD
+
+console.log(board.join('\n') + '\n\n');
+
+//Move King's Pawn forward 2
+board[4][4] = board[6][4];
+board[6][4] = ' ';
+console.log(board.join('\n'))
