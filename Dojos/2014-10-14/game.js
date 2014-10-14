9aa3b0a5f9e164b5726f2464ceb17fc26dba0e31
Kevin Kim <kimkvn@gmail.com>
2014-10-14 17:07:44 -0400

    @fab9 @rgcoders @kimkvn typed-copy game.js example

--- Dojos/2014-10-14/game.js
+++ Dojos/2014-10-14/game.js
@@ -2,4 +2,93 @@
  * Refactoring Conway...using objects. Egads.
  */
 
- 
+ module.exports = Game;
+
+ function board(){
+   return [
+   [false, false, false],
+   [false, false, false],
+   [false, false, false]
+   ];
+ }
+
+ function Game(){
+   this.board = board();
+ }
+
+ /**
+  * @param Number x coordinate
+  * @param Number y coordinate
+  * @return Boolean if cell at {x,y} is alive
+  */
+Game.prototype.isAlive = function(x,y){
+
+}
+
+/**
+ *Make the cell at {x,y} "alive", whatever that means...
+ *
+ * @param Number x coordinate
+ * @param Number y coordinate
+ * @return undefined
+ */
+Game.prototype.setAlive = function(x,y){
+
+}
+
+/**
+ * Update the 'board' by applying the 'rules' to each cell.
+ */
+Game.prototype.tick = function(){
+    //Start with a fresh board...
+    //Apply 'rules' to each cell in the current board...
+    //Record dthe result of 'rules' in the new board...
+    //Update the current board to match the new board.
+}
+
+/**
+ * What goes here?
+ */
+Game.prototype.rules = function(){
+
+}
+
+Game.prototype.rules = function(){
+
+}
+
+/**
+ * WARNING: This is VOODOO MAGIC...
+ *
+ * GIVEN:
+ *   this.board === [
+ *      [ false, true,  false ],
+ *      [ false, true,  false ],
+ *      [ false, true,  false ],
+ *   ];
+ *
+ * EXPECT:
+ *   +---+---+---+
+ *   |   | X |   |
+ *   +---+---+---+
+ *   |   | X |   |
+ *   +---+---+---+
+ *   |   | X |   |
+ *   +---+---+---+
+ */
+Game.prototype.display = function(){
+    var spacer = '+---+---+---+\n';
+
+    return spacer +
+        // Apply `renderRow` to each `row` in `board`...
+        this.board.map(function renderRow(row){
+            return '| ' +
+                // Apply `renderCell` to each `cell` in `row`...
+                row.map(function renderCell(cell){
+                    // return 'X' if `cell` is TRUTHY otherwise return ' '
+                    return cell && 'X' || ' ';
+                }).join(' | ') // Place ' | ' between each `cell`...
+            + ' |\n';
+        }).join(spacer) // Place `spacer` between each `row`...
+    + spacer;
+} // END display
