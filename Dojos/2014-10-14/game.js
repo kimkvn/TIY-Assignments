3ac24bbe397efdf57a06c913ce34a7d17557f137
Kevin Kim <kimkvn@gmail.com>
2014-10-15 10:59:58 -0400

    understanding how tick() is working...

--- Dojos/2014-10-14/game.js
+++ Dojos/2014-10-14/game.js
@@ -23,9 +23,8 @@ function Game(){
   */
 Game.prototype.isAlive = function(x,y){
 
-  if(this.board[x][y] === true){
-    return true;
-  }
+  return this.board[x][y];
+
 
 }
 
@@ -35,6 +34,7 @@ Game.prototype.isAlive = function(x,y){
  * @param Number x coordinate
  * @param Number y coordinate
  * @return undefined
+ * //this is a command, so you don't expect it to return anyting, simply perform an action
  */
 Game.prototype.setAlive = function(x,y){
 
@@ -47,53 +47,90 @@ Game.prototype.setAlive = function(x,y){
  */
 Game.prototype.tick = function(){
     //Start with a fresh board...
+    var newBoard = board();
     //Apply 'rules' to each cell in the current board...
+    this.board.forEach(function(row, x){  //refer to board. We apply .forEach to the rows of the board array first as it's the first part of the array. X = row index
+      row.forEach(function(item, y){ //refer to board, now we're applying .forEach to individual cells, the columnIndex (which = Y)
+
+      }) // END row.forEach
+    }); //END this.board.forEach
+
     //Record the result of 'rules' in the new board...
     //Update the current board to match the new board.
+
+
 }
 
 Game.prototype.neighborsOf = function(x,y){
   var neighbors;
+  var alive = 0;
+
   if(x === 0 && y === 0){
-    neighbors = [this.board[0][1], this.board[1][0], this.board[1][1]]
+    neighbors = [this.board[0][1], this.board[1][0], this.board[1][1]];
+    for(var i = 0; i < neighbors.length; i++){
+      if(neighbors[i] === true){alive+=1}
+      }
   };
 
   if(x === 0 && y === 1){
     neighbors = [this.board[0][0], this.board[1][2], this.board[1][0],
     this.board[1][1], this.board[1][2]];
-    for(var i = 0; i < neighbors.length; i++)
+    for(var i = 0; i < neighbors.length; i++){
+      if(neighbors[i] === true){alive+=1}
+      }
   };
 
   if(x === 0 && y === 2){
-    neighbors = [this.board[0][1], this.board[1][1], this.board[1][2]]
+    neighbors = [this.board[0][1], this.board[1][1], this.board[1][2]];
+    for(var i = 0; i < neighbors.length; i++){
+      if(neighbors[i] === true){alive+=1}
+      }
   };
-  
+
   if(x === 1 && y === 0){
-    neighbors = [this.board[0][0], this.board[0][1], this.board[1][1]]
+    neighbors = [this.board[0][0], this.board[0][1], this.board[1][1]];
+    for(var i = 0; i < neighbors.length; i++){
+      if(neighbors[i] === true){alive+=1}
+      }
   };
 
   if(x === 1 && y === 1){
     neighbors = [this.board[0][0], this.board[0][1], this.board[0][2],
     this.board[1][0], this.board[1][2], this.board[2][0], this.board[2][1],
-    this.board[2][2]]
+    this.board[2][2]];
+    for(var i = 0; i < neighbors.length; i++){
+      if(neighbors[i] === true){alive+=1}
+      }
   };
 
   if(x === 1 && y === 2){
     neighbors = [this.board[0][1], this.board[0][2], this.board[1][1],
-    this.board[2][1], this.board[2][2]]
+    this.board[2][1], this.board[2][2]];
+    for(var i = 0; i < neighbors.length; i++){
+      if(neighbors[i] === true){alive+=1}
+      }
   };
 
   if(x === 2 && y === 0){
-    neighbors = [this.board[1][0], this.board[1][1], this.board[2][1]]
+    neighbors = [this.board[1][0], this.board[1][1], this.board[2][1]];
+    for(var i = 0; i < neighbors.length; i++){
+      if(neighbors[i] === true){alive+=1}
+      }
   };
 
   if(x === 2 && y === 1){
     neighbors = [this.board[1][0], this.board[1][1], this.board[1][2],
-    this.board[2][0], this.board[2][2]]
+    this.board[2][0], this.board[2][2]];
+    for(var i = 0; i < neighbors.length; i++){
+      if(neighbors[i] === true){alive+=1}
+      }
   };
 
   if(x === 2 && y === 2){
-    neighbors = [this.board[1][1], this.board[1][2], this.board[2][1]]
+    neighbors = [this.board[1][1], this.board[1][2], this.board[2][1]];
+    for(var i = 0; i < neighbors.length; i++){
+      if(neighbors[i] === true){alive+=1}
+      }
   }
 
 
