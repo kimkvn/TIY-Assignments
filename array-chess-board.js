ef8d47961497938489b8bb9dc1ff7f43db2dded8
Kevin Kim <kimkvn@gmail.com>
2014-10-16 15:39:52 -0400

    @arigonzo, modifying my function to be object-oriented

--- array-chess-board.js
+++ array-chess-board.js
@@ -142,9 +142,87 @@ function moves(number, color){
 }// END numbers()
 
 
-var moves = {
+var catalanOpening = {
+  move1 : function(){
+    // //1 - White's move: Pawn to d4
+    board[4][3] = board[6][3];
+    board[6][3] = ' ';
+    // console.log(board.join('\n') + '\n\n');
+  },
 
+  move2 : function(){
+    // //1 - Black's move: Knight to f6
+    board[2][5] = board[0][6];
+    board[0][6] = ' ';
+    // console.log(board.join('\n') + '\n\n');
+  },
+
+  move3 : function(){
+    // //2 - White's move: Pawn to c4
+    board[4][2] = board[6][2];
+    board[6][2] = ' ';
+    // console.log(board.join('\n') + '\n\n');
+  },
+
+  move4: function(){
+    // //2 - Black's move: Pawn to e6
+    board[2][4] = board[1][4];
+     oard[1][4] = ' ';
+    // console.log(board.join('\n') + '\n\n');
+  },
+
+  move5 : function(){
+    // //3 - White's move: Pawn to g3
+    board[5][6] = board[6][6];
+    board[6][6] = ' ';
+    // console.log(board.join('\n') + '\n\n');
+  },
+
+  move6 : function(){
+    // //3 - Black's move: Pawn to d5
+    board[3][3] = board[1][3];
+    board[1][3] = ' ';
+    // console.log(board.join('\n') + '\n\n');
+  },
+
+  move7 : function(){
+    // //4 - White's move: Bishop to g2
+    board[6][6] = board[7][5];
+    board[7][5] = ' ';
+    // console.log(board.join('\n') + '\n\n');
+  },
+
+  move8 : function(){
+    // //4 - Black's move: Bishop to e7
+    board[1][4] = board[0][5];
+    board[0][5] = ' ';
+    // console.log(board.join('\n') + '\n\n');
+  },
+
+  move9 : function(){
+    // //5 - White's move: Knight to f3
+    board[5][5] = board[7][6];
+    board[7][6] = ' ';
+    // console.log(board.join('\n') + '\n\n');
+  }
 }//END moves
+
+
+
+//
+
+//
+
+//
+
+//
+
+//
+
+//
+
+//
+
 
 // console.log(moves(1, "white"))
 describe('checks the board status after each move', function(){
@@ -179,49 +257,6 @@ describe('checks the board status after each move', function(){
 
 
 
-// //1 - White's move: Pawn to d4
-// board[4][3] = board[6][3];
-// board[6][3] = ' ';
-// console.log(board.join('\n') + '\n\n');
-//
-// //1 - Black's move: Knight to f6
-// board[2][5] = board[0][6];
-// board[0][6] = ' ';
-// console.log(board.join('\n') + '\n\n');
-//
-// //2 - White's move: Pawn to c4
-// board[4][2] = board[6][2];
-// board[6][2] = ' ';
-// console.log(board.join('\n') + '\n\n');
-//
-// //2 - Black's move: Pawn to e6
-// board[2][4] = board[1][4];
-// board[1][4] = ' ';
-// console.log(board.join('\n') + '\n\n');
-//
-// //3 - White's move: Pawn to g3
-// board[5][6] = board[6][6];
-// board[6][6] = ' ';
-// console.log(board.join('\n') + '\n\n');
-//
-// //3 - Black's move: Pawn to d5
-// board[3][3] = board[1][3];
-// board[1][3] = ' ';
-// console.log(board.join('\n') + '\n\n');
-//
-// //4 - White's move: Bishop to g2
-// board[6][6] = board[7][5];
-// board[7][5] = ' ';
-// console.log(board.join('\n') + '\n\n');
-//
-// //4 - Black's move: Bishop to e7
-// board[1][4] = board[0][5];
-// board[0][5] = ' ';
-// console.log(board.join('\n') + '\n\n');
-//
-// //5 - White's move: Knight to f3
-// board[5][5] = board[7][6];
-// board[7][6] = ' ';
-// console.log(board.join('\n') + '\n\n');
+
 
 ///Testing///
