4ed75dcfc3c0a6110adf0c3f8cb473b368f3ef3e
Kevin Kim <kimkvn@gmail.com>
2014-10-17 11:47:16 -0400

    starting the outline

---
+++ Chess/14-Chessboard-Interface.js
@@ -0,0 +1,19 @@
+function Chess(){
+
+  getPlayer: function(){
+
+  },//END getPlayer
+
+  move: function(){
+
+  },//END move
+
+  opening: function(){
+
+  },//END opening
+
+  display: function(){
+
+  }//END display
+
+} //END Chess
