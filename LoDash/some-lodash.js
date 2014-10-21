dfa99e4f13e8e3293cbe29b625e09402de0f5e55
Kevin Kim <kimkvn@gmail.com>
2014-10-21 03:54:14 -0400

    wrote some stuff. Tests not working. bed for now.

--- LoDash/some-lodash.js
+++ LoDash/some-lodash.js
@@ -8,10 +8,29 @@
  */
 
 
+var some = function(collection, property){
+  var truthys = [ ];
+  collection.forEach(function(element, index, array){
+    if(callback(element) == true){
+    return true
+    }
+  })
+} //END some
 
-
- 
 
  //--Testing--//
 
  var expect = require('chai').expect;
+
+ describe('some function', function(){
+   it('should return a boolean', function(){
+
+     var collection = [4, 12, 7, 9, 16];
+     var callback = function(n){
+        if n > 5{
+          return true
+        }
+     }
+     expect(some(collection, callback)).to.be.true
+   });
+ })
