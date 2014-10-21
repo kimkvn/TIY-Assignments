4b0d2381150a483d4334f0bfbdc5efe82d0ed1b7
Kevin Kim <kimkvn@gmail.com>
2014-10-21 03:27:26 -0400

    initial commit for LoDash method naive implementation: some

---
+++ LoDash/some-lodash.js
@@ -0,0 +1,17 @@
+/* Naive implementation of Lo-Dash methods.
+ *
+ * Lo-Dash method: _.some
+ *
+ * It will check all elements of a collection. If any element is
+ * truthy after a callback argument/function is applied to it,
+ * _.some will return 'true'.
+ */
+
+
+
+
+ 
+
+ //--Testing--//
+
+ var expect = require('chai').expect;
