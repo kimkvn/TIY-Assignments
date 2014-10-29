c834bdf5966cd118c1b89a9b3b0fa63aeca1682d
Kevin Kim <kimkvn@gmail.com>
2014-10-28 21:11:12 -0400

    SO CLOSE. I just need it so when I click on a tab a second time, it closes itself.

--- HTML_CSS/20-wire-it-up/horizontalmenu.js
+++ HTML_CSS/20-wire-it-up/horizontalmenu.js
@@ -1,31 +1,45 @@
-function hello(){alert("hello")}
+/*
+* Javascript functions for horizontalmenu.html
+*/
 
 
 
-function openMenu(element){
-  document.getElementById(element).style.display = 'block';
+  function openMenu(element){
+    document.getElementById(element).style.display = 'block';
 };
 
-function closeMenu(element){
-  document.getElementById(element).style.display = 'none';
+  function closeMenu(element){
+    document.getElementById(element).style.display = 'none';
 };
 
 
-
-function toggleMenu(element){
-  if (document.getElementById(element).style.display === 'none'){
-    return openMenu(element);
-  }
-  return closeMenu(element);
-};//END toggleMenu
+var allMenus = ['products', 'downloads', 'applications', 'projects', 'freeware'];
 
 
+  function toggleMenu(element){
+      for(var i = 0; i<allMenus.length; i++){
+        if(allMenus[i] !== element){
+         closeMenu(allMenus[i]);
+       }
 
+
+      }
+return openMenu(element);
+  };
+
+
+
+
+
+ function selfToggle(element){
+   if(openMenu(element)){
+     return closeMenu(element);
+   }
+ }
+
+//this function will be in the header and makes it so clicking anywhere in the header resets the menus.
 function clearAll(){
-
-  var allMenus = ['products', 'downloads', 'applications', 'projects', 'freeware'];
-
     for( var i = 0; i < allMenus.length; i++){
-        document.getElementById(allMenus[i]).style.display = "none";
+        closeMenu(allMenus[i]);
     }
 }; //END clearAll
