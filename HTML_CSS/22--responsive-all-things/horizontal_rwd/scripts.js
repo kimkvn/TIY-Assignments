8dad61ec1baba70118ef97c96dbd8565acdc0654
Kevin Kim <kimkvn@gmail.com>
2014-10-31 10:16:50 -0400

    javascripts for horizontal dropdown menus

--- HTML_CSS/22--responsive-all-things/horizontal_rwd/scripts.js
+++ HTML_CSS/22--responsive-all-things/horizontal_rwd/scripts.js
@@ -12,3 +12,38 @@ function makeItOpen(element){
 function makeItClosed(element){
   element.className = '';
 }
+
+//checks if html element is open, returns boolean
+function isOpen(element){
+  if (element.className === 'open'){
+    return true;
+  }
+  return false;
+}
+
+//checks if html element is closed, returns boolean
+function isClosed(element){
+  if (element.className === ''){
+    return true;
+  }
+  return false;
+}
+
+
+//opens/closes menu tabs
+function clickNavItem(event){
+  if ( isClosed(this.parentElement)){
+    makeEverythingClosed(navItems);
+    makeItOpen(this.parentElement);
+  }
+    else if (isOpen(this.parentElement)){
+      makeItClosed(this.parentElement);
+    }
+}
+
+//makes everything closed on click
+function makeEverythingClosed(elements){
+ _.each(elements, function(element){
+   element.parentElement.className = '';
+ });
+}
