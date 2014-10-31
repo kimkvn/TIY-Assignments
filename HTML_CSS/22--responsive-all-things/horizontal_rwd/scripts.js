7fd58617966c0939cdeba284080d32f6614182dd
Kevin Kim <kimkvn@gmail.com>
2014-10-31 11:25:48 -0400

    javascripts still not working. For some reason, the basic functions work on h3, but it's not picking up the submenus...

--- HTML_CSS/22--responsive-all-things/horizontal_rwd/scripts.js
+++ HTML_CSS/22--responsive-all-things/horizontal_rwd/scripts.js
@@ -1,32 +1,6 @@
 /* javascripts for dropdown menus, responsive, mobile-first */
 
 
-//assigns chosen element the class 'open'
-function makeItOpen(element){
-  element.className = 'open';
-}
-
-//assigns chosen element an empty class
-function makeItClosed(element){
-  element.className = '';
-}
-
-//checks if html element is open, returns boolean
-function isOpen(element){
-  if (element.className === 'open'){
-    return true;
-  }
-  return false;
-}
-
-//checks if html element is closed, returns boolean
-function isClosed(element){
-  if (element.className === ''){
-    return true;
-  }
-  return false;
-}
-
 
 //opens/closes menu tabs
 function clickNavItem(event){
@@ -38,6 +12,37 @@ function clickNavItem(event){
       makeItClosed(this.parentElement);
     }
 }
+
+
+//checks if html element is open, returns boolean
+function isOpen(element){
+  if (element.className === 'open'){
+    return true;
+  }
+  return false;
+}
+
+
+//checks if html element is closed, returns boolean
+function isClosed(element){
+  if (element.className === ''){
+    return true;
+  }
+  return false;
+}
+
+
+
+//assigns chosen element the class 'open'
+function makeItOpen(element){
+  element.className = 'open';
+}
+
+//assigns chosen element an empty class
+function makeItClosed(element){
+  element.className = '';
+}
+
 
 //makes everything closed on click
 function makeEverythingClosed(elements){
