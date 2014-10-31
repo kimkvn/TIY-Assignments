72255a92b20b62e6733b56160cd8d00319bf52d5
Kevin Kim <kimkvn@gmail.com>
2014-10-31 02:53:19 -0400

    added more things to group lists, added prelim javascripts

--- HTML_CSS/22--responsive-all-things/horizontal_rwd/scripts.js
+++ HTML_CSS/22--responsive-all-things/horizontal_rwd/scripts.js
@@ -1,1 +1,14 @@
 /* javascripts for dropdown menus, responsive, mobile-first */
+
+var navItems = document.getElementsByTagName('h3'); //collects all h3 tags and assigns them to array navItems
+
+
+//assigns chosen element the class 'open'
+function makeItOpen(element){
+  element.className = 'open';
+}
+
+//assigns chosen element an empty class
+function makeItClosed(element){
+  element.className = '';
+}
