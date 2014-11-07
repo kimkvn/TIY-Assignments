8cddcd1b97f96a5929e9a8b67ecd62eaf8a6faba
Kevin Kim <kimkvn@gmail.com>
2014-11-06 20:56:24 -0500

    building up the gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,14 @@
 var gulp = require('gulp');
+webserver = require('gulp-webserver');
+connect = require('gulp-connect');
+
+
+gulp.task('connect', function(){
+  connect.server();
+});
 
 gulp.task('greeting', function(){
   console.log('hello');
 });
 
-gulp.task('default', ['greeting']);
+gulp.task('default', ['greeting', 'connect']);
