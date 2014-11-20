ff4197407c2f69e267d56bfd1100350e8ee0ffdc
Kevin Kim <kimkvn@gmail.com>
2014-11-20 18:17:14 -0500

    ok. I was wondering why my jquery script links weren't working, because the boostrap tabs function wasn't working. Turns out I had to order the jquery links at the top of the script links block. Doh

--- GH_Profile/angularScripts.js
+++ GH_Profile/angularScripts.js
@@ -1,25 +1,35 @@
 (function(){
 
-  // var app = angular.module('profile', []);
+  var app = angular.module('profile', []);
+
+    app.controller('userController', ['$http', function($http){
+
+      var user = this;
+      user.info = [];
+      $http.get('kimkvn.json').success(function(data){
+        user.info = data;
+
+      });
+    }]);
+
+  // var app =
   //
-  //   app.controller('userController', ['$http', function($http){
+  // angular.module('profile').controller('userController', function(Restangular){
   //
-  //     var user = this;
-  //     user.info = [];
-  //     $http.get('kimkvn.json').success(function(data){
-  //       user.info = data;
+  //   Restangular.setBaseUrl('https://api.github.com');
   //
-  //     });
-  //   }]);
-
-  var app =
-
-  angular.module('profile').controller('userController', function(Restangular){
-
-    Restangular.setBaseUrl('https://api.github.com');
-
-    this.user = Restangular.one('users', 'kimkvn').get().$object;
-
-    });
+  //   // this.user = Restangular.one('users', 'kimkvn').get().$object;
+  //
+  //   var User = Restangular.one('users', 'kimkvn');
+  //
+  //   var userPromise = User.get();
+  //
+  //   this.user = userPormise.$object;
+  // 
+  //   var UserRepos = User.all('repos');
+  //
+  //   this.repos = UserRepos.getList().$object;
+  //
+  //   });
 
 })();
