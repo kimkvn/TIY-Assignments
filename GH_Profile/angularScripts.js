df0569e9e79ab92fe8bf9697b63aa2acebfcd00b
Kevin Kim <kimkvn@gmail.com>
2014-11-20 00:36:47 -0500

    bower installed restangular, attempting to implement in gh profile

--- GH_Profile/angularScripts.js
+++ GH_Profile/angularScripts.js
@@ -1,17 +1,18 @@
 (function(){
 
-  var app = angular.module('profile', []);
+  // var app = angular.module('profile', []);
+  //
+  //   app.controller('userController', ['$http', function($http){
+  //
+  //     var user = this;
+  //     user.info = [];
+  //     $http.get('kimkvn.json').success(function(data){
+  //       user.info = data;
+  //
+  //     });
+  //   }]);
 
-    app.controller('userController', ['$http', function($http){
-
-      var user = this;
-      user.info = [];
-      $http.get('kimkvn.json').success(function(data){
-        user.info = data;
-
-      });
-    }]);
-
+  
 
 
 })();
