20efe152a99f364c1ab6bbd2b0a7153ce01f85ca
Kevin Kim <kimkvn@gmail.com>
2014-11-19 20:01:02 -0500

    fuck. why the fuck can't I GET the api data

--- GH_Profile/angularScripts.js
+++ GH_Profile/angularScripts.js
@@ -1,5 +1,16 @@
 (function(){
-  
+
+  var app = angular.module('githubProfile', []);
+
+    app.controller('UserController', ['$http', function($http){
+
+      user.info = [];
+      $http.get('/kimkvn.json').success(function(data){
+        user.info = data;
+
+      });
+    }]);
+
 
 
 })();
