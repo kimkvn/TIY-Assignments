1834dd6e53586e618234fc2a4495498f6d561e1c
Kevin Kim <kimkvn@gmail.com>
2014-11-21 14:38:22 -0500

    more restangular wrangling

--- GH_Profile/angularScripts.js
+++ GH_Profile/angularScripts.js
@@ -1,35 +1,56 @@
-(function(){
+// (function(){
+//
+//   var app = angular.module('profile', []);
+//
+//     app.controller('userController', ['$http', function($http){
+//
+//       var user = this;
+//       user.info = [];
+//       $http.get('kimkvn.json').success(function(data){
+//         user.info = data;
+//
+//       });
+//     }]);
+//
+// })();
 
-  var app = angular.module('profile', []);
+'use-strict';
 
-    app.controller('userController', ['$http', function($http){
+angular.module('profile').constant('API', {
+  base: 'https://api.github.com'
+})
+  .run(function(Restangular, API){
+    Restangular.setBaseUrl(API.base);
+  })
+    .factory('Users', function(Restangular){
+      return Restangular.service('users');
+    })
+      .controller('userController', function(Users){
 
-      var user = this;
-      user.info = [];
-      $http.get('kimkvn.json').success(function(data){
-        user.info = data;
+        var User =  Users.one('kimkvn');
 
+        this.user = User.get().$object;
+
+        this.user.repos = Use.all('repos').getList().$object;
+        
       });
-    }]);
 
-  // var app =
-  //
-  // angular.module('profile').controller('userController', function(Restangular){
-  //
-  //   Restangular.setBaseUrl('https://api.github.com');
-  //
-  //   // this.user = Restangular.one('users', 'kimkvn').get().$object;
-  //
-  //   var User = Restangular.one('users', 'kimkvn');
-  //
-  //   var userPromise = User.get();
-  //
-  //   this.user = userPormise.$object;
-  // 
-  //   var UserRepos = User.all('repos');
-  //
-  //   this.repos = UserRepos.getList().$object;
-  //
-  //   });
-
-})();
+// var app =
+//
+//angular.module('profile').controller('userController', function(Restangular){
+//
+//   Restangular.setBaseUrl('https://api.github.com');
+//
+//   // this.user = Restangular.one('users', 'kimkvn').get().$object;
+//
+//   var User = Restangular.one('users', 'kimkvn');
+//
+//   var userPromise = User.get();
+//
+//   this.user = userPormise.$object;
+//
+//   var UserRepos = User.all('repos');
+//
+//   this.repos = UserRepos.getList().$object;
+//
+//   });
